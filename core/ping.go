eb1fddbcdfd3cb05f55ef3ec3dc87b782f013149
Theredwiking <hjulmandn@gmail.com>
2024-08-01 22:11:22 +0200

    Working example

--- core/ping.go
+++ core/ping.go
@@ -10,36 +10,37 @@ import (
 
 // Pings device from string, uses 2 channels
 // so as many can be started and used with only 2 channels
-func Ping(in chan string, out chan bool) {
+func Ping(in chan string, out chan models.Device) {
 	for {
 		select {
 		case ip := <- in:
-			device := models.Device{ip, false, []}
+			device := models.Device{Ip: ip, Active: false, Ports: []int{}}
 			pinger, err := ping.NewPinger(ip)
 			if err != nil {
 				log.Println(err)
-				out <- false
+				out <- device
 				return
 			}
 			
 			pinger.SetPrivileged(true)
 			pinger.Count = 2
-			pinger.Timeout = 5 * time.Second
+			pinger.Timeout = 2 * time.Second
 
 			err = pinger.Run()
 			if err != nil {
 				pinger.Stop()
 				log.Println(err)
-				out <- false
+				out <- device
 				return
 			}
 
 			stats := pinger.Statistics()
 
 			if stats.PacketLoss == 0 {
-				out <- true
+				device.Active = true
+				out <- device
 			} else {
-				out <- false
+				out <- device
 			}
 		}
 	}
--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/theredwiking/lanscan/core"
+	"github.com/theredwiking/lanscan/models"
 )
 
 func main() {
-	ips := make(chan string)
-	alive := make(chan bool, 254)
+	ips := make(chan string, 254)
+	alive := make(chan models.Device, 254)
 	
 	ipRange := []string{"192.168.0.1", "192.168.0.7", "192.168.0.8", "192.168.0.9", "192.168.0.10"}
 	
@@ -18,14 +20,26 @@ func main() {
 
 	ips <- ipRange[0]
 	ipRange = append(ipRange[:0], ipRange[1:]...)
+	ips <- ipRange[0]
+	ipRange = append(ipRange[:0], ipRange[1:]...)
+	ips <- ipRange[0]
+	ipRange = append(ipRange[:0], ipRange[1:]...)
+
+	ticker := time.NewTicker(500 * time.Millisecond)
 
 	for {
 		select {
 		case msg := <-alive:
 			log.Println(msg)
-			ips <- ipRange[0]
-			if len(ipRange) > 1 {
-				ipRange = append(ipRange[:0], ipRange[1:]...)
+		case _ = <- ticker.C:
+			if len(ipRange) != 0 {
+				ips <- ipRange[0]
+				log.Println(len(ipRange))
+				if len(ipRange) >= 2 {
+					ipRange = append(ipRange[:0], ipRange[1:]...)
+				} else {
+					ipRange = []string{}
+				}
 			}
 		}
 	}
